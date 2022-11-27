bf6dbc40c80f71b83ed17f2f1208909bc7c87fcb
0za0 <43097509+0za0@users.noreply.github.com>
2022-11-27 03:00:58 +0100

    Ree

---
+++ BingoOnline/Models/Settings.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BingoOnline.Models
+{
+    //User DI for this
+    public class Settings
+    {
+        public readonly string SettingsPath = Path.Combine(Directory.GetCurrentDirectory(),".config");
+        public Color P1_Clicked { get; private set; }
+        public Color P1_NonClicked { get; private set; }
+        public Color ButtonFontColor { get; private set; }
+        public Color P2_Clicked { get; private set; }
+
+        public async Task LoadSettings()
+        {
+            await File.ReadAllLinesAsync(SettingsPath);
+        }
+
+        public async Task SaveSettings()
+        {
+            using FileStream createStream = File.Create(SettingsPath);
+            await JsonSerializer.SerializeAsync(createStream, this);
+            await createStream.DisposeAsync();
+        }
+    }
+}
